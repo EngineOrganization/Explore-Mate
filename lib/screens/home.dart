d6b39a1a7da53d0f1ed1793af1c3329e556a561c
JuLoSpace <julospace@gmail.com>
2024-06-22 21:39:55 +0500

    update maps

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,9 @@
 import 'package:explore_mate/screens/map.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class HomeScreen extends StatefulWidget {
 
@@ -11,8 +14,20 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
-  int count_times = 6;
+  int count_times = 10;
   int count_travels = 3;
+  bool time_is_open = false;
+
+  late YandexMapController controller;
+  GlobalKey mapKey = GlobalKey();
+  Future<bool> get locationPermissionNotGranted async => !(await Permission.location.request().isGranted);
+
+  final List<ChartData> chartData = [
+    ChartData('чебуречная', 1),
+    ChartData('абоба', 2),
+    ChartData('баобаб', 5),
+    ChartData('шаурма', 7),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +35,7 @@ class _HomeScreenState extends State<HomeScreen> {
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: ListView(
+        physics: PageScrollPhysics(),
         children: [
           Container(
             padding: EdgeInsets.all(2),
@@ -70,10 +86,10 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           Container(
             margin: EdgeInsets.only(left: width * 0.1, right: width * 0.1),
-            height: height * 0.2,
+            height: time_is_open ? (count_times + 1) * 20 : 6 * 20,
             child: ListView.builder(
               physics: NeverScrollableScrollPhysics(),
-              itemCount: count_times,
+              itemCount: time_is_open ? count_times : 5,
               itemBuilder: (context, index) {
                 return Container(
                   margin: EdgeInsets.only(top: 2),
@@ -82,7 +98,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
-                        color: Colors.grey.withOpacity(1 / (index + 1)),
+                        color: time_is_open ? Colors.grey : Colors.grey.withOpacity(1 / (index + 1)),
                         height: 3,
                         width: width * 0.65,
                       ),
@@ -96,16 +112,49 @@ class _HomeScreenState extends State<HomeScreen> {
           Container(
             height: height * 0.025,
             margin: EdgeInsets.only(left: width * 0.4, right: width * 0.4),
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.circular(30)
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black
+              ),
+              onPressed: () {
+                setState(() {
+                  time_is_open = !time_is_open;
+                });
+              },
+              child: Icon(time_is_open ? Icons.arrow_upward_outlined : Icons.arrow_downward_outlined, color: Colors.white, size: 18,),
             ),
-            child: Icon(Icons.arrow_downward_outlined, color: Colors.white, size: 18,),
           ),
           Container(
             margin: EdgeInsets.only(left: width * 0.05, right: width * 0.05, top: height * 0.01),
-            height: height * 0.3,
+            height: height * 0.2,
             child: YandexMap(
+              key: mapKey,
+              onMapCreated: (YandexMapController yandexMapController) async {
+                controller = yandexMapController;
+                final mediaQuery = MediaQuery.of(context);
+                final height = mapKey.currentContext!.size!.height * mediaQuery.devicePixelRatio;
+                final width = mapKey.currentContext!.size!.width * mediaQuery.devicePixelRatio;
+                await controller.toggleUserLayer(
+                    visible: true,
+                    autoZoomEnabled: true,
+                    anchor: UserLocationAnchor(
+                        course: Offset(1 * width, 0.5 * height),
+                        normal: Offset(0.5 * width, 0.5 * height)
+                    )
+                );
+              },
+              onUserLocationAdded: (UserLocationView view) async {
+                return view.copyWith(
+                    pin: view.pin.copyWith(
+                        icon: PlacemarkIcon.single(
+                            PlacemarkIconStyle(image: BitmapDescriptor.fromAssetImage('assets/user.png'))
+                        )
+                    ),
+                    accuracyCircle: view.accuracyCircle.copyWith(
+                        fillColor: Colors.green.withOpacity(0.5)
+                    )
+                );
+              },
               onMapTap: (Point) {
                 Navigator.of(context).push(MaterialPageRoute(builder: (context) => MapScreen()));
               },
@@ -133,8 +182,50 @@ class _HomeScreenState extends State<HomeScreen> {
               },
             ),
           ),
+          Container(
+            alignment: Alignment.topRight,
+            margin: EdgeInsets.only(right: width * 0.05, top: height * 0.01),
+            child: Text('Рейтинг кафе в вашем городе', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+          ),
+          Container(
+            height: height * 0.4,
+            margin: EdgeInsets.only(left: width * 0.05, right: width * 0.05),
+            child: SfCartesianChart(
+              margin: EdgeInsets.only(top: 0),
+              plotAreaBackgroundColor: Colors.transparent,
+              borderColor: Colors.transparent,
+              borderWidth: 0,
+              plotAreaBorderWidth: 0,
+              enableSideBySideSeriesPlacement: false,
+              primaryXAxis: CategoryAxis(
+                arrangeByIndex: true,
+                majorGridLines: MajorGridLines(width: 0),
+                majorTickLines: MajorTickLines(width: 0),
+              ),
+              primaryYAxis: CategoryAxis(
+                isVisible: false,
+                maximum: chartData.last.y + 1,
+              ),
+              series: [
+                ColumnSeries<ChartData, String>(
+                  dataSource: chartData,
+                  xValueMapper: (ChartData sales, _) => sales.x,
+                  yValueMapper: (ChartData sales, _) => sales.y,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(4), topRight: Radius.circular(4)),
+                  color: Color(0xFF5e6488),
+                ),
+              ],
+            ),
+          )
         ],
       )
     );
   }
+}
+
+
+class ChartData {
+  ChartData(this.x, this.y);
+  final String x;
+  final int y;
 }
--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
-
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class MapScreen extends StatefulWidget {
 
@@ -11,10 +12,53 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
 
+  late YandexMapController controller;
+  GlobalKey mapKey = GlobalKey();
+  Future<bool> get locationPermissionNotGranted async => !(await Permission.location.request().isGranted);
+
+  @override
+  void initState() {
+    super.initState();
+    getPosition();
+  }
+
+  void getPosition() async {
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.medium);
+    print(position.latitude);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: YandexMap(),
+      body: YandexMap(
+        key: mapKey,
+        onMapCreated: (YandexMapController yandexMapController) async {
+          controller = yandexMapController;
+          final mediaQuery = MediaQuery.of(context);
+          final height = mapKey.currentContext!.size!.height * mediaQuery.devicePixelRatio;
+          final width = mapKey.currentContext!.size!.width * mediaQuery.devicePixelRatio;
+          await controller.toggleUserLayer(
+              visible: true,
+              autoZoomEnabled: true,
+              anchor: UserLocationAnchor(
+                  course: Offset(1 * width, 0.5 * height),
+                  normal: Offset(0.5 * width, 0.5 * height)
+              )
+          );
+        },
+        onUserLocationAdded: (UserLocationView view) async {
+          return view.copyWith(
+            pin: view.pin.copyWith(
+              icon: PlacemarkIcon.single(
+                PlacemarkIconStyle(image: BitmapDescriptor.fromAssetImage('assets/user.png'))
+              )
+            ),
+              accuracyCircle: view.accuracyCircle.copyWith(
+                  fillColor: Colors.green.withOpacity(0.5)
+              )
+          );
+        },
+      )
     );
   }
 }
